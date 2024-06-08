ab80f1e82e287876520d541a7c29eecd93057fdf
saifakib <mdsaifakib@gmail.com>
2024-06-08 16:12:54 +0600

    added paginations packages

---
+++ src/packages/paginator/src/index.ts
@@ -0,0 +1,4 @@
+import { paginator } from './paginator';
+import * as PaginatorTypes from '@/packages/paginator/types';
+
+export { paginator, PaginatorTypes };
---
+++ src/packages/paginator/src/paginator.ts
@@ -0,0 +1,46 @@
+import * as PaginatorTypes from '@/packages/paginator/types';
+
+const getOrderByWithTieBreaker = (args: any = {}, defaultOptions: PaginatorTypes.PaginateOptions = {}) => {
+  const { orderByTieBreakerPropertyName } = defaultOptions;
+
+  if (!orderByTieBreakerPropertyName || !args.orderBy) {
+    return args.orderBy;
+  }
+
+  return [
+    ...(args.orderBy instanceof Array ? args.orderBy : [args.orderBy]),
+    { [orderByTieBreakerPropertyName]: 'asc' },
+  ];
+};
+
+export const paginator = (defaultOptions: PaginatorTypes.PaginateOptions): PaginatorTypes.PaginateFunction => {
+  return async (model, args: any = { where: undefined }, options) => {
+    const page = Number(options?.page || defaultOptions?.page) || 1;
+    const perPage = Number(options?.perPage || defaultOptions?.perPage) || 10;
+    const orderBy = getOrderByWithTieBreaker(args, defaultOptions);
+
+    const skip = page > 0 ? perPage * (page - 1) : 0;
+    const [total, data] = await Promise.all([
+      model.count({ where: args.where }),
+      model.findMany({
+        ...args,
+        orderBy,
+        take: perPage,
+        skip,
+      }),
+    ]);
+    const lastPage = Math.ceil(total / perPage);
+
+    return {
+      data,
+      meta: {
+        total,
+        lastPage,
+        currentPage: page,
+        perPage,
+        prev: page > 1 ? page - 1 : null,
+        next: page < lastPage ? page + 1 : null,
+      },
+    };
+  };
+};
